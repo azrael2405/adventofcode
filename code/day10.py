63aae662e8dd38ed83fc9ab72794bd53e36c01d7
Manuel Köll <man.koell2405@gmail.com>
2022-12-10 14:35:10 +0100

    day 10, answer 2

--- code/day10.py
+++ code/day10.py
@@ -2,6 +2,9 @@
 class CPU:
     def __init__(self):
         self.register_x = [1]
+        self.crt = ['#']
+        self.crt_width = 40
+        
         
     def addx(self, value):
         self.register_x.append(self.register_x[-1])
@@ -9,6 +12,23 @@ class CPU:
         
     def noop(self):
         self.register_x.append(self.register_x[-1])
+
+    def calculate_crt(self):
+        for cycle, sprite_pos in enumerate(self.register_x[1:], 1):
+            sprite_diff = sprite_pos - (cycle % self.crt_width)
+            if sprite_diff >= -1 and sprite_diff <= 1:
+                self.crt.append('#')
+            else:
+                self.crt.append('.')
+
+    def draw_crt(self):
+        step = self.crt_width
+        print('answer2:\n{}'.format(
+            '\n'.join([
+                ''.join(self.crt[start:start+step])
+                for start in range(0, len(self.crt), step)
+            ])
+        ))
 
 
 if __name__ == '__main__':
@@ -25,11 +45,11 @@ if __name__ == '__main__':
             cpu.noop()
 
     answer1_cycles = [20,60,100,140,180,220]
-    print(cpu.register_x)
-    print(f'cycle 20: {cpu.register_x[20]}')
     answer1 = sum([
         a_cycle * cpu.register_x[a_cycle-1]
         for a_cycle in answer1_cycles
     ])
     print(f'answer1: {answer1}')
+    cpu.calculate_crt()
+    cpu.draw_crt()
 
