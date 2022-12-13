65f452b84bbb33149771b4ce83c4266afb7f605a
Manuel Köll <mko@quorum.at>
2022-12-13 10:42:35 +0100

    day 13, answer 1

--- code/day13.py
+++ code/day13.py
@@ -1,3 +1,31 @@
+import ast
+
+def check_in_order(left, right):
+    answer = None
+    
+    for index, left_item in enumerate(left):
+        if len(right) <= index:
+            return False
+        right_item = right[index]
+        if type(left_item) != type(right_item):
+            if type(left_item) != list:
+                left_item = [left_item]
+            elif type(right_item) != list:
+                right_item = [right_item]
+        if type(left_item) == type(right_item) == int:
+            if left_item < right_item:
+                return True
+            elif right_item < left_item:
+                return False
+        elif type(left_item) == type(right_item) == list:
+            answer = check_in_order(left_item, right_item)
+            if answer in (True, False):
+                return answer
+    if len(left) < len(right):
+        return True
+    return answer
+        
+
 
 if __name__ == '__main__':
     input = ''
@@ -5,5 +33,14 @@ if __name__ == '__main__':
     with open(f'../data/day{day}.txt', 'r') as iFile:
         input = iFile.read()
 
-    
+    index_sum = 0
+    for index, data_lines in enumerate(input.split('\n\n'), 1):
+        if not data_lines: continue
+        (left_str, right_str) = data_lines.split('\n')
+        left = ast.literal_eval(left_str)
+        right = ast.literal_eval(right_str)
+        if check_in_order(left, right):
+            index_sum += index
+
+    print(f'answer1: {index_sum}')
 
