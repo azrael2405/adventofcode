d5b36b852828682e271e0744669c84891db3be41
Manuel Köll <man.koell2405@gmail.com>
2022-12-18 09:51:55 +0100

    day 18, answer 1

--- code/day18.py
+++ code/day18.py
@@ -1,3 +1,20 @@
+def check_sides(cube_list: list[tuple[int, int, int]], x: int, y: int, z: int) -> int:
+    open_sides = 0
+    if (x+1,y,z) not in cube_list:
+        open_sides += 1
+    if (x-1,y,z) not in cube_list:
+        open_sides += 1
+    if (x,y+1,z) not in cube_list:
+        open_sides += 1
+    if (x,y-1,z) not in cube_list:
+        open_sides += 1
+    if (x,y,z+1) not in cube_list:
+        open_sides += 1
+    if (x,y,z-1) not in cube_list:
+        open_sides += 1
+    
+    return open_sides
+
 
 if __name__ == '__main__':
     input = ''
@@ -5,4 +22,14 @@ if __name__ == '__main__':
     with open(f'../data/day{day}.txt', 'r') as iFile:
         input = iFile.read()
 
+cube_list = []
+open_sides = 0
+for line in input.splitlines():
+    (x, y, z) = line.split(',')
+    cube_list.append((int(x),int(y),int(z)))
+
+for cube in cube_list:
+    open_sides += check_sides(cube_list, cube[0],cube[1],cube[2])
+
+print(f'answer1: {open_sides}')
 
