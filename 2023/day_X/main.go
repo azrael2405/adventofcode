af52e455095a3124f920dc488bfcaaedb573880c
Manuel Köll <man.koell2405@gmail.com>
2023-12-02 13:17:02 +0100

    chore: rework bootstraping func and package names

--- 2023/day_X/main.go
+++ 2023/day_X/main.go
@@ -1,4 +1,4 @@
-package dayX
+package main
 
 import (
 	"fmt"
--- 2023/init_day.sh
+++ 2023/init_day.sh
@@ -1,5 +1,4 @@
 
 cp -r day_X day_$1
 sed -i 's/dayX/day'$1'/' day_$1/go.mod
-sed -i 's/dayX/day'$1'/' day_$1/main.go
 sed -i 's/use (/use (\n\t.\/day_'$1'/' go.work
