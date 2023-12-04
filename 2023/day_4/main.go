192cc147f9e6104713167c6777316e27058506f3
Manuel Köll <man.koell2405@gmail.com>
2023-12-04 13:47:37 +0100

    staring day 4

---
+++ 2023/day_4/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"helper"
+	"os"
+	"strings"
+	"time"
+)
+
+func check_error(e error){
+	if e != nil{
+		panic(e)
+	}
+}
+
+func parse_input_from_file(filepath string) []string{
+	file_data, file_error := os.ReadFile(filepath)
+	check_error(file_error)
+	lines := strings.Fields(string(file_data))
+	return lines
+}
+
+
+func parse_answer_one(_data []string){
+	defer helper.TimeTrack(time.Now(), "Answer 1")
+	answer := ""
+	fmt.Println("Answer 1:", answer)
+}
+
+
+func parse_answer_two(_data []string){
+	defer helper.TimeTrack(time.Now(), "Answer 2")
+	answer := ""
+	fmt.Println("Answer 2:", answer)
+	
+}
+
+func main (){
+	defer helper.TimeTrack(time.Now(), "main")
+	filepath := os.Args[1]
+	data_array := parse_input_from_file(filepath)
+	parse_answer_one(data_array)
+	parse_answer_two(data_array)
+}
+
+
--- 2023/init_day.sh
+++ 2023/init_day.sh
@@ -2,3 +2,5 @@
 cp -r day_X day_$1
 sed -i 's/dayX/day'$1'/' day_$1/go.mod
 sed -i 's/use (/use (\n\t.\/day_'$1'/' go.work
+git add .
+git commit -m "staring day "$1
