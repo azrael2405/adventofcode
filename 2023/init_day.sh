6abde0a39b2131adcceed30b3c425ecde58c3d85
Manuel Köll <man.koell2405@gmail.com>
2023-12-04 14:05:43 +0100

    updated init script

--- 2023/init_day.sh
+++ 2023/init_day.sh
@@ -1,7 +1,11 @@
 
 cp -r day_X day_$1
+mv day_$1/dayX day_$1/day$1
 sed -i 's/dayX/day'$1'/' day_$1/go.mod
 sed -i 's/use (/use (\n\t.\/day_'$1'/' go.work
+sed -i 's/dayX/day'$1'/g' day_$1/main.go
+sed -i 's/dayX/day'$1'/g' day_$1/day$1/Part1.go
+sed -i 's/dayX/day'$1'/g' day_$1/day$1/Part2.go
 git add .
 git commit -m "feat: chore staring day "$1
 git push
