f9d52e81cbcf470e6c270bf0888b8285b3b0e10b
Manuel Köll <man.koell2405@gmail.com>
2023-12-04 13:49:17 +0100

    updated initiation script

--- 2023/init_day.sh
+++ 2023/init_day.sh
@@ -3,4 +3,6 @@ cp -r day_X day_$1
 sed -i 's/dayX/day'$1'/' day_$1/go.mod
 sed -i 's/use (/use (\n\t.\/day_'$1'/' go.work
 git add .
-git commit -m "staring day "$1
+git commit -m "feat: chore staring day "$1
+git push
+cd day_$1
