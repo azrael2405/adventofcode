6512ab716cb72900e737a5a0cfdef52ee290e0e4
Manuel Köll <man.koell2405@gmail.com>
2023-12-05 18:10:01 +0100

    feat: chore staring day 5

---
+++ 2023/day_5/day5/Part1.go
@@ -0,0 +1,14 @@
+package day5
+
+import (
+	"fmt"
+	"helper"
+	"time"
+)
+
+
+func Parse_answer_one(_data []string){
+	defer helper.TimeTrack(time.Now(), "Answer 1")
+	answer := ""
+	fmt.Println("Answer 1:", answer)
+}
---
+++ 2023/day_5/day5/Part2.go
@@ -0,0 +1,13 @@
+package day5
+
+import (
+	"fmt"
+	"helper"
+	"time"
+)
+
+func parse_answer_two(_data []string){
+	defer helper.TimeTrack(time.Now(), "Answer 2")
+	answer := ""
+	fmt.Println("Answer 2:", answer)
+}
