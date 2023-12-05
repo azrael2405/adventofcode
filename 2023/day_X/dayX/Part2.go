2e41c1c5ac08c66dbc619f9ea5984e37776e5bd9
Manuel Köll <man.koell2405@gmail.com>
2023-12-05 18:11:15 +0100

    fix: wrong naming of part2 export

--- 2023/day_X/dayX/Part2.go
+++ 2023/day_X/dayX/Part2.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func parse_answer_two(_data []string){
+func Parse_answer_two(_data []string){
 	defer helper.TimeTrack(time.Now(), "Answer 2")
 	answer := ""
 	fmt.Println("Answer 2:", answer)
