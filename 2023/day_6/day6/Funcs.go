86808a43f5cfe327c8dd0b033977a6a73f7c179e
Manuel Köll <man.koell2405@gmail.com>
2023-12-06 19:36:36 +0100

    finished day 6 part 1

--- 2023/day_6/day6/Funcs.go
+++ 2023/day_6/day6/Funcs.go
@@ -1,1 +1,35 @@
 package day6
+
+import (
+	"helper"
+	"regexp"
+	"strconv"
+)
+
+type race_type struct{
+	time int
+	distance int
+}
+
+func is_even(_number int) bool{
+	return _number % 2 > 0
+}
+
+func parse_data(_data_array []string) []*race_type{
+	races := []*race_type{}
+	re := regexp.MustCompile(`\d+`)
+	times := re.FindAllString(_data_array[0], -1)
+	distances := re.FindAllString(_data_array[1], -1)
+	for index := range times{
+		time, conversion_error := strconv.Atoi(times[index])
+		helper.Check_error((conversion_error))
+		distance, conversion_error := strconv.Atoi(distances[index])
+		helper.Check_error((conversion_error))
+		new_race := race_type{
+			time: time,
+			distance: distance,
+		}
+		races = append(races, &new_race)
+	}
+	return races
+}
--- 2023/day_6/day6/Part1.go
+++ 2023/day_6/day6/Part1.go
@@ -6,9 +6,26 @@ import (
 	"time"
 )
 
+func parse_winning_moves(_races []*race_type) int{
+	answer := 1
+	for _, race := range _races{
+		winning_moves := 0
+		start_time := 1
+		for i := start_time; i < race.time; i++{
+			if i * (race.time-i) > race.distance{
+				winning_moves += 1
+			}
+		}
+		fmt.Println(race.time, race.distance, winning_moves)
+		answer *= winning_moves
+	}
+	return answer
+}
+
 
 func Parse_answer_one(_data []string){
 	defer helper.TimeTrack(time.Now(), "Answer 1")
-	answer := ""
+	races := parse_data(_data)
+	answer := parse_winning_moves(races)
 	fmt.Println("Answer 1:", answer)
 }
