78b3d0bc8ad149b7d43a649b07e5e9a39d57c398
Manuel Köll <man.koell2405@gmail.com>
2023-12-06 19:01:22 +0100

    feat: starting day 6

---
+++ 2023/day_6/day6/Part2.go
@@ -0,0 +1,13 @@
+package day6
+
+import (
+	"fmt"
+	"helper"
+	"time"
+)
+
+func Parse_answer_two(_data []string){
+	defer helper.TimeTrack(time.Now(), "Answer 2")
+	answer := ""
+	fmt.Println("Answer 2:", answer)
+}
---
+++ 2023/day_6/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"day6/day6"
+	"helper"
+	"os"
+	"strings"
+	"time"
+)
+
+func parse_input_from_file(filepath string) []string{
+	file_data, file_error := os.ReadFile(filepath)
+	helper.Check_error(file_error)
+	lines := strings.Split(string(file_data), "\n")
+	return lines
+}
+
+
+func main (){
+	defer helper.TimeTrack(time.Now(), "main")
+	filepath := os.Args[1]
+	data_array := parse_input_from_file(filepath)
+	day6.Parse_answer_one(data_array)
+	day6.Parse_answer_two(data_array)
+}
+
+
