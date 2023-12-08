5f8081ebad952a9750871750e2e54386f2858ac7
Manuel Köll <man.koell2405@gmail.com>
2023-12-08 13:43:39 +0100

    feat: starting day 8

---
+++ 2023/day_8/day8/day8_test.go
@@ -0,0 +1,34 @@
+package day8
+
+import (
+	"helper"
+	"os"
+	"strings"
+	"testing"
+)
+
+func parse_input_from_file(filepath string) []string{
+	file_data, file_error := os.ReadFile(filepath)
+	helper.Check_error(file_error)
+	lines := strings.Split(string(file_data), "\n")
+	return lines
+}
+func Test_part1(t *testing.T){
+	test_data := parse_input_from_file(`E:\projects\adventofcode2022\2023\day_8\test1.txt`)
+	expected_answer := 6440
+	answer := Parse_answer_one(test_data)
+	if answer != expected_answer{
+		t.Fatalf("Part1 Test does not conform to expected answer: %d / %d\n", answer, expected_answer)
+		
+	}
+}
+
+func Test_part2(t *testing.T){
+	test_data := parse_input_from_file(`E:\projects\adventofcode2022\2023\day_8\test1.txt`)
+	expected_answer := 5905
+	answer := Parse_answer_two(test_data)
+	if answer != expected_answer{
+		t.Fatalf("Part2 Test does not conform to expected answer: %d / %d\n", answer, expected_answer)
+		
+	}
+}
---
+++ 2023/day_8/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"day8/day8"
+	"helper"
+	"os"
+	"strings"
+	"time"
+)
+
+func parse_input_from_file(filepath string) []string{
+	file_data, file_error := os.ReadFile(filepath)
+	helper.Check_error(file_error)
+	lines := strings.Split(string(file_data), "\n")
+	return lines
+}
+
+
+func main (){
+	defer helper.TimeTrack(time.Now(), "main")
+	filepath := os.Args[1]
+	data_array := parse_input_from_file(filepath)
+	day8.Parse_answer_one(data_array)
+	day8.Parse_answer_two(data_array)
+}
+
+
