c3994d4430f70de5f7e257421fee404cc8becd7e
Manuel Köll <man.koell2405@gmail.com>
2023-12-09 09:49:28 +0100

    feat: starting day 9

---
+++ 2023/day_9/day9/Part1.go
@@ -0,0 +1,15 @@
+package day9
+
+import (
+	"fmt"
+	"helper"
+	"time"
+)
+
+
+func Parse_answer_one(_data []string) int{
+	defer helper.TimeTrack(time.Now(), "Answer 1")
+	answer := 0
+	fmt.Println("Answer 1:", answer)
+	return 0
+}
