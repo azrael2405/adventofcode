f2a24dbf27b1ccfc58e331cd60c59993e8157b86
Manuel Köll <man.koell2405@gmail.com>
2023-12-16 09:43:11 +0100

    feat: starting day 14

---
+++ 2023/day_14/day14/Part1.go
@@ -0,0 +1,15 @@
+package day14
+
+import (
+	"fmt"
+	"helper"
+	"time"
+)
+
+
+func Parse_answer_one(_data []string) int{
+	defer helper.TimeTrack(time.Now(), "Answer 1")
+	answer := 0
+	fmt.Println("Answer 1:", answer)
+	return answer
+}
---
+++ 2023/day_14/day14/Part2.go
@@ -0,0 +1,14 @@
+package day14
+
+import (
+	"fmt"
+	"helper"
+	"time"
+)
+
+func Parse_answer_two(_data []string)int{
+	defer helper.TimeTrack(time.Now(), "Answer 2")
+	answer := 0
+	fmt.Println("Answer 2:", answer)
+	return answer
+}
